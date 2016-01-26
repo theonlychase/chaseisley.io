d31e2eea4fb25bba3743df74eb5c32cac97c5c29
Chase Isley <isleychase@gmail.com>
2016-01-26 15:04:08 -0700

    Added node server. Moved assets folder to public folder. Tested and working

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var cors        = require('cors');
+var morgan      = require('morgan');
+var path        = require('path');
+
+var port 	    = process.env.PORT || 8080;
+// var port 	    = process.env.PORT || 80;
+
+// get our request parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors
+app.use(cors());
+
+// log to console
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+// Start the server
+app.listen(port);
+console.log('Chase Isley Port: http://localhost:' + port);
