c68317c92181390198cf5162a0599d44a8691ea2
Chase Isley <isleychase@gmail.com>
2016-01-26 17:14:00 -0700

    Removed console error

--- public/js/controllers/UserProfileController.js
+++ public/js/controllers/UserProfileController.js
@@ -1,7 +1,7 @@
 angular.module('MetronicApp').controller('UserProfileController', function($rootScope, $scope, $http, $timeout) {
     $scope.$on('$viewContentLoaded', function() {   
         App.initAjax(); // initialize core components
-        Layout.setSidebarMenuActiveLink('set', $('#sidebar_menu_link_profile')); // set profile link active in sidebar menu 
+        // Layout.setSidebarMenuActiveLink('set', $('#sidebar_menu_link_profile')); // set profile link active in sidebar menu 
     });
 
     // set sidebar closed and body solid layout mode
